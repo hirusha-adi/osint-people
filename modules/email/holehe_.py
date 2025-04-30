cb3fe38d311c1ab991c24d4c434da1d6de28199d
hirusha-adi <hirushaadi@gmail.com>
2025-04-30 09:31:39 +0530

    show err status at end

--- modules/email/holehe_.py
+++ modules/email/holehe_.py
@@ -1,12 +1,16 @@
 import os
 from utils import decorators
+from utils import colored
 
 
 @decorators.handle_errors
 def start(email: str, color=False, clear=False) -> None:
-    try:
-        import holehe  
-    except ImportError:
-        os.system("pip install holehe" if os.name == 'nt' else "pip3 install holehe")
+    colored.print_debug(f"Running holehe with email: {email}")
     
-    os.system(f"holehe {'--no-color' if not color else ''} {'--no-clear' if not clear else ''} {email}")
+    cmd = f"holehe {'--no-color' if not color else ''} {'--no-clear' if not clear else ''} {email}"
+    exit_code = os.system(cmd)
+
+    if exit_code == 0:
+        colored.print_success("Successfully ran holehe.")
+    else:
+        colored.print_error("An error occurred while running holehe.")
