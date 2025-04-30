1ddddf37a68827778272b7fb0c3ea996f4c72e19
hirusha-adi <hirushaadi@gmail.com>
2025-04-30 09:47:53 +0530

    re-factor

--- modules/email/specific/chess_com.py
+++ modules/email/specific/chess_com.py
@@ -1,12 +1,20 @@
-import requests
-from utils import decorators
-
+from utils import decorators, colored, request
 
 @decorators.handle_errors
-def start(email: str):
-    r = requests.get(f"https://www.chess.com/callback/email/available?email={email}")
+def start(email: str) -> None:
+    colored.print_debug(f"Checking chess.com for email: {email}")
 
-    if r.json()["isEmailAvailable"] == True:
-        print(f"[-] Account not found in chess.com.")
-    elif r.json()["isEmailAvailable"] == False:
-        print(f"[+] Account found in chess.com.")
+    # f"https://www.chess.com/callback/email/available?email={email}"
+    url = f"https://www.chess.com/callback/email/available"
+    data = request.safe_request("GET", url, params={"email": email}, result_as_json=True)
+    
+    if data is None:
+        colored.print_error("No data returned from chess.com.")
+        return
+
+    if data.get("isEmailAvailable") is True:
+        colored.print_warning("[-] Account not found in chess.com.")
+    elif data.get("isEmailAvailable") is False:
+        colored.print_success("[+] Account found in chess.com.")
+    else:
+        colored.print_error("Unexpected response format from chess.com.")
--- modules/email/specific/duolingo.py
+++ modules/email/specific/duolingo.py
@@ -1,21 +1,26 @@
-import requests
-from utils import decorators
-
+from utils import decorators, colored, request
 
 @decorators.handle_errors
-def start(email: str):
+def start(email: str) -> None:
+    colored.print_debug(f"Checking duolingo for email: {email}")
 
-    r = requests.get(
-        "https://www.duolingo.com/2017-06-30/users", params={"email": email}
-    )
+    # f"https://www.duolingo.com/2017-06-30/users?email={email}"
+    url = "https://www.duolingo.com/2017-06-30/users"
+    data = request.safe_request("GET", url, params={"email": email}, result_as_json=True)
+
+    if data is None:
+        colored.print_error("No data returned from duolingo.")
+        return
     
-    if """{"users":[]}""" in r.text:
-        print("[-] Account not found in doulingo.")
-
+    if not data.get("users"):
+        colored.print_warning("Account not found in duolingo.")
     else:
-        username = r.json()["users"][0]["username"]
-        if username is not None and username != "":
-            print("[+] Account found in duolingo, with username: {username}")
+        first_user: dict = data.get("users")[0]
+        if first_user:
+            username = first_user.get("username", None)
+            if username:
+                colored.print_success(f"Account found in duolingo, with username: {username}")
+            else:
+                colored.print_warning("Account not found in duolingo.")
         else:
-            print("[-] Account not found in duolingo.")
-
+            colored.print_warning("Account not found in duolingo.")
