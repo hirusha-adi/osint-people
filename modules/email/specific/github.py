cdf1f599f77d50f70c0d801b739ceb056b9bf3f4
hirusha-adi <hirushaadi@gmail.com>
2025-04-30 10:13:45 +0530

    refactor

--- modules/email/specific/github.py
+++ modules/email/specific/github.py
@@ -1,36 +1,50 @@
-import requests
-from utils import decorators
-
+import typing as t
+from utils import decorators, colored, request
 
 @decorators.handle_errors
-def start(email: str):
-    r = requests.get(f"https://api.github.com/search/users?q={email}+in:email")
+def start(email: str) -> None:
+    colored.print_info(f"Checking GitHub for email: {email}")
 
-    data = r.json()
+    # f"https://api.github.com/search/users?q={email}+in:email"
+    url = "https://api.github.com/search/users"
+    params = {"q": f"{email} in:email"}
+    data = request.safe_request("GET", url, params=params, result_as_json=True)
+
+    if data is None:
+        colored.print_error("No data returned from GitHub.")
+        return
+
     total_count = data.get("total_count", 0)
 
     if total_count == 0:
-        print("[-] Account not found in Github")
-    else:
-        print(f"[+] Accounts found in GitHub: {total_count}")
-        print("----------")
-        for item in data.get("items", []):
-            print("Username:", item.get("login"))
-            print("ID:", item.get("id"))
-            print("Node ID:", item.get("node_id"))
-            print("Avatar URL:", item.get("avatar_url"))
-            print("Gravatar ID:", item.get("gravatar_id"))
-            print("Profile URL:", item.get("html_url"))
-            print("Followers URL:", item.get("followers_url"))
-            print("Following URL:", item.get("following_url"))
-            print("Gists URL:", item.get("gists_url"))
-            print("Starred URL:", item.get("starred_url"))
-            print("Subscriptions URL:", item.get("subscriptions_url"))
-            print("Organizations URL:", item.get("organizations_url"))
-            print("Repos URL:", item.get("repos_url"))
-            print("Events URL:", item.get("events_url"))
-            print("Received Events URL:", item.get("received_events_url"))
-            print("Type:", item.get("type"))
-            print("Site Admin:", item.get("site_admin"))
-            print("Score:", item.get("score"))
+        colored.print_warning("Account not found in GitHub.")
+        return
+
+    colored.print_success(f"Accounts found in GitHub: {total_count}")
+    print("----------")
+
+    data: t.List[t.Union[None,t.Dict]] = data.get("items", [])
+    for item in data:
+        if item:
+            print(f"Username: {item.get('login')}")
+            print(f"ID: {item.get('id')}")
+            print(f"Node ID: {item.get('node_id')}")
+            print(f"Avatar URL: {item.get('avatar_url')}")
+            print(f"Gravatar ID: {item.get('gravatar_id')}")
+            print(f"Profile URL: {item.get('html_url')}")
+            print(f"Followers URL: {item.get('followers_url')}")
+            print(f"Following URL: {item.get('following_url')}")
+            print(f"Gists URL: {item.get('gists_url')}")
+            print(f"Starred URL: {item.get('starred_url')}")
+            print(f"Subscriptions URL: {item.get('subscriptions_url')}")
+            print(f"Organizations URL: {item.get('organizations_url')}")
+            print(f"Repos URL: {item.get('repos_url')}")
+            print(f"Events URL: {item.get('events_url')}")
+            print(f"Received Events URL: {item.get('received_events_url')}")
+            print(f"Type: {item.get('type')}")
+            print(f"Site Admin: {item.get('site_admin')}")
+            print(f"Score: {item.get('score')}")
             print("----------")
+
+    if len(data) == 0:
+        print("----------")
